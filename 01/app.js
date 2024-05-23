b37255d77be9e862cd0d522b6ebee0da33ff039b
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-05-23 18:16:51 +0200

    task 01 all done

---
+++ 01/app.js
@@ -0,0 +1,8 @@
+//działa 
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(React.createElement('h1', null, 'React działa!'));
