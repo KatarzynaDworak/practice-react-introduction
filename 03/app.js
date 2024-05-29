6fc91e21aa292ee514bd54ad75047c6dfd370148
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-05-29 14:51:41 +0200

    task 03 partly done

--- 03/app.js
+++ 03/app.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import MenuItem from './components/MenuItem';
 
 const root = createRoot(document.querySelector('#root'));
 
-// 1. przy wykorzystaniu funkcji:
-// const App = () => {
-//     return (
-//     <header>
-//         Moja pierwsza strona w React
-//     </header>
-//     );
-// }
-
-// 2. przy wykorzystaniu klasy z ES2015
-
-class Header extends React.Component {
-    render() {
-        return <header>Moja pierwsza strona w React
-        </header>
-    }
-}
 const App = () => {
-        return ( 
-        <div>
-            <Header />
-        </div>
-        );
- }
+    return (
+    <MenuItem>TEST</MenuItem>
+    );
+}
+
+
 
 root.render(<App />);
