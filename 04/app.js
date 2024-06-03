826622bc1e48c95a7b9faf0ee9a7d6a4a321b0e8
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-06-03 18:07:39 +0200

    task 04 all done

--- 04/app.js
+++ 04/app.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import MenuItem from './components/MenuItem';
+import MenuItem from '../03/components/MenuItem';
 
 const root = createRoot(document.querySelector('#root'));
 
-const App = () => {
+const Menu = () => {
     return (
-        <div>
-            <MenuItem />
-        </div>
-    );
+        <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="/contact">kontakt</a></li>
+        </ul>
+    )
 }
 
 const Nav = () => {
     return (
         <nav>
-            <ul>
-                <li><a href="/">home</a></li>
-                <li><a href="/contact">kontakt</a></li>
-            </ul>
+            <Menu></Menu>
         </nav>  
     )
 }
 
-root.render(<App />);
+root.render(<Nav />);
